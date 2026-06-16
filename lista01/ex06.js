19ee9ed71bd3232c2b26fbb37894da0f90b3fe90
Ruan Pablo <ruan.pablo.ramos@escola.pr.gov.br>
2026-06-16 14:33:34 -0300

    Update ex06.js

--- lista01/ex06.js
+++ lista01/ex06.js
@@ -1,7 +1,7 @@
 // Contagem regressiva (Requer: npm install readline-sync)
 const readline = require('readline-sync');
 
-const numero = parseInt(readline.question("Digite um numero para a contagem regressiva: "));
+const numero = number(input.question("Digite um numero para a contagem regressiva: "));
 
 for (let i = numero; i >= 1; i--) {
     console.log(i);
